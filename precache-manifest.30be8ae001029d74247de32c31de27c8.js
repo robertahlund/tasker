87a600dff482b2187a7b5f901fe7078665f870cc
Robert Åhlund <robert@MBPsomtorRobert.lan>
2020-09-05 00:07:41 +0200

    Updates

---
+++ precache-manifest.30be8ae001029d74247de32c31de27c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30d3277e938243ef8f00970a5780b82",
+    "url": "/tasker/index.html"
+  },
+  {
+    "revision": "8bb7e6800ba132bd3e68",
+    "url": "/tasker/static/css/main.467b9168.chunk.css"
+  },
+  {
+    "revision": "996835d22dc89b7f654d",
+    "url": "/tasker/static/js/2.266a3bb6.chunk.js"
+  },
+  {
+    "revision": "6cac0f2d6fd8ecbe8ffbe3f8ff0d29f4",
+    "url": "/tasker/static/js/2.266a3bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bb7e6800ba132bd3e68",
+    "url": "/tasker/static/js/main.f95788d2.chunk.js"
+  },
+  {
+    "revision": "86ac9c551ace4ae08080",
+    "url": "/tasker/static/js/runtime-main.ad7c467e.js"
+  }
+]);
